d560f7cb347d2403e41a652e29ccfecc03404d89
Kitty Rose Hamilton <kitty@flarenet.co.uk>
2022-07-26 22:28:50 +0100

    v.0.1

---
+++ Raiders_Jester/Raiders_Jester/Program.cs
@@ -0,0 +1,73 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Raiders_Jester
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            client.MessageReceived += CommandHandler;
+            client.Log += Log;
+
+            YamlSerialization.Config_Yaml();
+
+            await client.LoginAsync(TokenType.Bot, YamlSerialization.token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task<Task> CommandHandler(SocketMessage message)
+        {
+            string command = "";
+            int lengthOfCommand = -1;
+
+            if (!message.Content.StartsWith("!"))
+                return Task.CompletedTask;
+
+            if (message.Author.IsBot)
+                return Task.CompletedTask;
+
+            if (message.Content.Contains(' '))
+                lengthOfCommand = message.Content.IndexOf(' ');
+            else
+                lengthOfCommand = message.Content.Length;
+
+            command = message.Content.Substring(1, lengthOfCommand - 1).ToLower();
+
+            while(true)
+            {
+                Serverstatus.Serverping_DCSServer();
+                Task.Delay(TimeSpan.FromMinutes(5));
+
+                
+            }
+
+            if (command.Equals("server"))
+            {
+                Serverstatus.Serverping_DCSServer();
+
+                message.Channel.SendMessageAsync(message.Author.Mention + " Pinging Server... " + "\n" + Serverstatus.Serverping_DCSServer);
+            }
+
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
---
+++ Raiders_Jester/Raiders_Jester/Serverstatus.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace Raiders_Jester
+{
+    public class Serverstatus
+    {
+        public static string serverstat_dcsserver;
+        public static string ipaddress = Convert.ToString(YamlSerialization.ipaddress);
+        public static int port = Convert.ToInt32(YamlSerialization.port);
+        
+
+        
+
+       
+
+        
+        
+
+
+        public static async void Serverping_DCSServer()
+        {
+            TcpClient tcpClient = new TcpClient();
+            
+
+            if (tcpClient.ConnectAsync(IPAddress.Parse(ipaddress), port).Wait(1000))
+            {
+                
+                serverstat_dcsserver = "332nd Raiders Server is online";
+                tcpClient.Close();
+            }
+
+            else
+            {
+                
+                serverstat_dcsserver = "332nd Raiders Server is offline";
+            }
+
+
+
+        }
+
+
+    }
+
+
+
+}
