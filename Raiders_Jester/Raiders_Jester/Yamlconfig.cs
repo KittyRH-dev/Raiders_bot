69bded7b2179a98faa4899cef51d0b893e2a270b
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:57:43 +0000

    v.0.1

---
+++ Raiders_Jester/Raiders_Jester/Yamlconfig.cs
@@ -0,0 +1,15 @@
+﻿namespace Raiders_Jester
+{
+    public class Yamlconfig
+    {
+        public class botconfig
+        {
+            public string token { get; set; }
+            public string ipaddress { get; set; }
+            public string port { get; set; }
+        }
+
+        
+    }
+
+}
